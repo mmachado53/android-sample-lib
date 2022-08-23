3dd10df40280b63353073e2ff4ee2c497fabc6e8
Miguel Machado <mmachado53@gmail.com>
2022-08-23 17:23:00 -0600

    a simulated bugfix

--- mylibrary/src/main/java/wayakdev/sample/mylibrary/MySampleUtils.kt
+++ mylibrary/src/main/java/wayakdev/sample/mylibrary/MySampleUtils.kt
@@ -4,5 +4,5 @@ import android.app.Activity
 import android.widget.Toast
 
 fun Activity.toastHello(){
-    Toast.makeText(this, "Hello :) !!", Toast.LENGTH_LONG).show()
+    Toast.makeText(this, "Hello :) !!!!!", Toast.LENGTH_LONG).show()
 }
