ae69a6a2d393948d430a883ef0de6ee30197ebd6
Miguel Machado <mmachado53@gmail.com>
2023-01-23 20:52:40 -0700

    another fix

--- version-code-action/src/main.ts
+++ version-code-action/src/main.ts
@@ -1,12 +1,21 @@
 import {LastVersionCode} from './googleApiHelper.js';
 import * as core from '@actions/core';
+import { version } from 'os';
 
 const serviceAccountJsonFile = core.getInput("serviceAccountJsonFile", { required: true });
 const packageName = core.getInput("packageName", { required: true });
 
 try{
-    const versionCode = LastVersionCode(packageName, serviceAccountJsonFile);
-    core.setOutput("versionCode", versionCode);
+    LastVersionCode(packageName, serviceAccountJsonFile).then((versionCode)=> {
+        core.setOutput("versionCode", versionCode);
+    }).catch((error)=>{
+        if(error instanceof Error){
+            core.setFailed(error.message);
+        }else {
+            core.setFailed("Unknown error");
+        }
+    });
+    
 } catch (error){
     if(error instanceof Error){
         core.setFailed(error.message);
